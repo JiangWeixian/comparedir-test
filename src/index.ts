e0bc325566eab5d10501b441e4dbd0e481d10c22
JiangWeixian <jiangweixian1994@gmail.com>
2021-10-26 18:51:43 +0800

    :sparkles:  provide issamedir

--- src/index.ts
+++ src/index.ts
@@ -14,38 +14,55 @@ export const glob = (cwd: string) => {
  * @param target expected snapshot dir path
  * @param _options
  */
-export const compare = async (input: string, target: string, _options?: any) => {
-  if ('expect' in global) {
-    try {
-      expect.extend({ toMatchFile })
-    } catch (e) {}
-  }
+export const isSameDir = async (input: string, target: string, _options?: any) => {
   const [inputFiles, targetFiles] = await Promise.all(
     [input, target].map((cwd) => {
       return glob(cwd)
     }),
   )
   if (inputFiles.length !== targetFiles.length) {
-    if ('expect' in global) {
-      expect(inputFiles.length).toBe(targetFiles.length)
-    }
     return false
   }
   for (const inputFilePath of inputFiles) {
     const targetFilePath = path.resolve(target, inputFilePath)
     if (!fs.existsSync(targetFilePath)) {
-      if ('expect' in global) {
-        expect(fs.existsSync(targetFilePath)).toBe(true)
-      }
       return false
     }
     const inputBuffer = fs.readFileSync(path.resolve(input, inputFilePath))
     const targetBuffer = fs.readFileSync(targetFilePath)
-    if ('expect' in global) {
-      expect(inputBuffer.toString()).toMatchFile(targetFilePath)
-    }
+    // if not in jest env, early return false if not equal
     if (!inputBuffer.equals(targetBuffer)) {
       return false
+    }
+  }
+  return true
+}
+
+/**
+ * compare input and target dir is same or not
+ * @param input current dir path
+ * @param target expected snapshot dir path
+ * @param _options
+ */
+export const compare = async (input: string, target: string, _options?: any) => {
+  if ('expect' in global) {
+    try {
+      expect.extend({ toMatchFile })
+    } catch (e) {}
+  }
+  const [inputFiles] = await Promise.all(
+    [input, target].map((cwd) => {
+      return glob(cwd)
+    }),
+  )
+  for (const inputFilePath of inputFiles) {
+    const targetFilePath = path.resolve(target, inputFilePath)
+    const inputBuffer = fs.readFileSync(path.resolve(input, inputFilePath))
+    if ('expect' in global) {
+      if (!fs.existsSync(targetFilePath)) {
+        expect(fs.existsSync(targetFilePath)).toBe(true)
+      }
+      expect(inputBuffer.toString()).toMatchFile(targetFilePath)
     }
   }
   return true
